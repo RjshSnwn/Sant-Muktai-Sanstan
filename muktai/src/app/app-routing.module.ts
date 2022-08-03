d724c25b74453a226079e807c560cb0517bf82a9
Rajesh Sonawane <annuraj261998@gmail.com>
2022-08-03 09:00:36 +0530

    done full routing

--- muktai/src/app/app-routing.module.ts
+++ muktai/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LiteratureComponent } from './components/literature/literature.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { MediaComponent } from './components/media/media.component';
+import { OnlineServiceComponent } from './components/online-service/online-service.component';
+import { TendersComponent } from './components/tenders/tenders.component';
 import { TourismComponent } from './components/tourism/tourism.component';
 
 const routes: Routes = [{ 
@@ -11,7 +14,11 @@ const routes: Routes = [{
 { path: 'contact', component: ContactComponent },
 { path: 'about', component: AboutUsComponent },
 { path: 'tourism', component: TourismComponent},
-{ path:'literature', component: LiteratureComponent}];
+{ path:'literature', component: LiteratureComponent},
+{ path:'online', component: OnlineServiceComponent},
+{ path:'media', component: MediaComponent},
+{ path:'tenders', component: TendersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
